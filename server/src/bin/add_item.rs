03e7bf03d89a849878c3b6ce0cb34e12cd630b35
Afiq Nazrie <afnazrie@gmail.com>
2023-05-03 11:27:43 +0800

    feat: update add_item
    Use randomized item name for test

--- server/src/bin/add_item.rs
+++ server/src/bin/add_item.rs
@@ -1,12 +1,16 @@
 use diesel::prelude::*;
+use encrypt_password::encrypt_password;
+use rand::distributions::{Alphanumeric, DistString};
+use rand::thread_rng;
 use server::models::*;
 use server::*;
+
+mod encrypt_password;
 
 pub fn add_item(name: &str, password: &str) -> usize {
     use crate::schema::item;
 
     let conn = &mut establish_connection();
-
     let new_item = NewItem { name, password };
 
     diesel::insert_into(item::table)
@@ -16,5 +20,11 @@ pub fn add_item(name: &str, password: &str) -> usize {
 }
 
 fn main() {
-    add_item("foo", "bar");
+    let rand_string: String = Alphanumeric.sample_string(&mut thread_rng(), 8);
+    let password = "bar";
+
+    println!("Name: {rand_string}");
+    println!("Password: {password}");
+
+    add_item(rand_string.as_str(), encrypt_password(password).as_str());
 }
