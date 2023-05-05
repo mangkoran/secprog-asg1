28d2913918b058d11d6192fce765c1d301ceaeb2
Afiq Nazrie <afnazrie@gmail.com>
2023-05-05 22:37:54 +0800

    feat: initial api impl

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,12 +1,26 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::serde::json::Json;
+use server::{db::*, models::User};
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[post("/user/create", format = "json", data = "<user>")]
+fn user(user: Json<User>) {
+    println!("Received username: {}", user.username);
+    println!("Received password: {}", user.password);
+
+    let added_user = add_user(user.username.as_str(), user.password.as_str());
+
+    println!("Added username: {}", added_user.username);
+    println!("Added password: {}", added_user.password);
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, user])
 }
